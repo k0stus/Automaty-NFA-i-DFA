4e12222bce569f0345e8786cff0f9a7a2e04b4d3
k0stus <konstangens@gmail.com>
2025-06-10 09:54:38 +0200

    testy

--- tests/MinimizeDFATest.hs
+++ tests/MinimizeDFATest.hs
@@ -32,8 +32,37 @@ redundantDFA1 = DFA.DFA
   , DFA.acceptStates = Set.fromList [2, 3, 4]
   }
 
-redundantDFA2 :: DFA.DFA
 redundantDFA2 = DFA.DFA
+  { DFA.states = Set.fromList [0,1,2,3,4]
+  , DFA.alphabet = Set.fromList ['x']
+  , DFA.transition = Map.fromList
+      [ ((0, 'x'), 1)
+      , ((1, 'x'), 2)
+      , ((2, 'x'), 2)
+      -- stany 3 i 4 są martwe
+      , ((3, 'x'), 4)
+      , ((4, 'x'), 3)
+      ]
+  , DFA.startState = 0
+  , DFA.acceptStates = Set.fromList [2]
+  }
+
+redundantDFA3 = DFA.DFA
+  { DFA.states = Set.fromList [0, 1, 2, 3]
+  , DFA.alphabet = Set.fromList ['a']
+  , DFA.transition = Map.fromList
+      [ ((0, 'a'), 1)
+      , ((1, 'a'), 2)
+      , ((2, 'a'), 3)
+      , ((3, 'a'), 3)
+      ]
+  , DFA.startState = 0
+  , DFA.acceptStates = Set.fromList [2, 3]
+  }
+-- Stany 2 i 3 są nierozróżnialne — zawsze prowadzą do stanu akceptującego.
+
+redundantDFA4 :: DFA.DFA
+redundantDFA4 = DFA.DFA
   { DFA.states = Set.fromList [1, 2, 3, 4, 5, 6, 7]
   , DFA.alphabet = Set.fromList ['a', 'b']
   , DFA.transition = 
@@ -44,7 +73,7 @@ redundantDFA2 = DFA.DFA
       , ((3, 'a'), 4)
       , ((4, 'b'), 5)
       , ((5, 'a'), 2)
-      , ((2, 'b'), 4)
+      , ((5, 'b'), 4)
       ] |> Map.fromList
   , DFA.startState = 1
   , DFA.acceptStates = Set.fromList [4, 5]
@@ -56,21 +85,18 @@ x |> f = f x
 main :: IO ()
 
 main = do
+  putStrLn "\n=== DFA # 1 ==="
   putStrLn $ prettyPrintDFA redundantDFA1
   putStrLn(prettyPrintDFA(minimizeDFA redundantDFA1))
+  putStrLn     "==============="
+  putStrLn "\n\n=== DFA # 2 ==="
   putStrLn $ prettyPrintDFA redundantDFA2
   putStrLn(prettyPrintDFA(minimizeDFA redundantDFA2))
-
-{-
-main = hspec $ do
-  describe "DFA minimization" $ do
-    it "removes redundant states from a simple DFA" $ do
-      let minimized = minimizeDFA redundantDFA
-          minimizedStates = DFA.states minimized
-      Set.size minimizedStates `shouldBe` 3
-
-    it "retains correct accepting behavior" $ do
-      let minimized = minimizeDFA redundantDFA
-      DFA.acceptsDFA minimized "aa" `shouldBe` True
-      DFA.acceptsDFA minimized "a"  `shouldBe` False
-  -}    
+  putStrLn     "==============="
+  putStrLn "\n\n=== DFA # 3 ==="
+  putStrLn $ prettyPrintDFA redundantDFA3
+  putStrLn(prettyPrintDFA(minimizeDFA redundantDFA3))
+  putStrLn     "==============="
+  putStrLn "\n\n=== DFA # 4 ==="
+  putStrLn $ prettyPrintDFA redundantDFA4
+  putStrLn(prettyPrintDFA(minimizeDFA redundantDFA4))
